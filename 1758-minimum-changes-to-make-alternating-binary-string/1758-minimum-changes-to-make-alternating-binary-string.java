cd473fefcc6427c2ecfe49b32d805c1f51fd2988
Varsha Ramesh <50179010+varsharamesh82@users.noreply.github.com>
2022-04-05 17:27:21 +0530

    Time: 7 ms (26.97%), Space: 42.9 MB (60.86%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(String s) {
+        int count0=0;
+        int count1=0;
+        
+        for(int i=0;i<s.length();i++){
+          if((i%2==0 && s.charAt(i)=='0') || (i%2!=0 && s.charAt(i)!='0'))
+             count1++;
+        }
+             count0=s.length()-count1;
+             return Math.min(count0,count1);
+      
+    }
+}
